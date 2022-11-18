b94d6c906b1f382a87fcaad8d170d67d1506765f
Keita354 <113056447+Keita354@users.noreply.github.com>
2022-11-18 10:07:18 -0800

    fin

--- assignment2.py
+++ assignment2.py
@@ -1,2 +1,2 @@
 #! python3
-print('This is my second program. \nIt uses \"two commands\" to display the output.')this world  shall know pain
+print('This is my second program. \nIt uses \"two commands\" to display the output.')
